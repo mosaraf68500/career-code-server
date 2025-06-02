88dfca9250037311d061658253025cd1d71e0d86
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-06-02 23:43:28 +0600

    get all jobs

--- index.js
+++ index.js
@@ -27,10 +27,22 @@ async function run() {
   try {
     
     await client.connect();
-    
+
+    const jobCollection=client.db('carrerCode').collection('jobs');
+
+    // api
+    app.get("/jobs",async(req,res)=>{
+        const result= await jobCollection.find().toArray();
+        res.send(result);
+
+    })
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
+  } finally { 
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
