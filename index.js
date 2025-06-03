fc38cbb83b327f7b7c5b02c208e80a8d15aecfbe
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-06-03 13:32:13 +0600

    find one

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app=express();
 const port=process.env.PORT||3000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 // middleware
@@ -32,11 +32,19 @@ async function run() {
 
     // api
     app.get("/jobs",async(req,res)=>{
-        const result= await jobCollection.find().toArray();
+        const cursor=jobCollection.find();
+        const result =await cursor.toArray();
         res.send(result);
 
     })
 
+
+    app.get("/jobs/:id",async(req,res)=>{
+        const id=req.params.id;
+        const query={_id:new ObjectId(id)};
+        const result=await jobCollection.findOne(query);
+        res.send(result)
+    })
 
 
 
