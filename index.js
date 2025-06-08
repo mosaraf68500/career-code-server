b5c280f57855b42cff8f0519a2c92aeb96242e2c
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-06-08 13:31:18 +0600

    cookes

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-const jwt=require("jsonwebtoken");
-const app=express();
-const port=process.env.PORT||3000;
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const app = express();
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.use(cookieParser())
 app.use(express.json());
-
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nobizgo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -21,86 +24,88 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    
     await client.connect();
 
-    const jobCollection=client.db('carrerCode').collection('jobs');
-    const applicaitonsCollection=client.db('carrerCode').collection('apply');
-
-
+    const jobCollection = client.db("carrerCode").collection("jobs");
+    const applicaitonsCollection = client.db("carrerCode").collection("apply");
 
     // jwt token related api
 
-    app.post("/jwt",async(req,res)=>{
-        const {email}=req.body;
-        const user={email}
-        const token=jwt.sign(user,"secret",{expiresIn:"1h"});
-        res.send({token});
-    })
+    app.post("/jwt", async (req, res) => {
+      const userData = req.body;
+      console.log(userData)
+      const token = jwt.sign(userData, process.env.JWT_ACCESS_SECRET, {
+        expiresIn: "1d",
+      });
+     
+      // set cookies
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: false,
+         sameSite: "lax", 
+      });
+       console.log(token)
+      res.send({ success: true });
+    });
 
     // api
-    app.get("/jobs",async(req,res)=>{
+    app.get("/jobs", async (req, res) => {
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.hr_email = email;
+      }
+      const cursor = jobCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-        const email=req.query.email;
-        const query={};
-        if(email){
-            query.hr_email=email
-        }
-        const cursor=jobCollection.find();
-        const result =await cursor.toArray();
-        res.send(result);
+    app.post("/jobs", async (req, res) => {
+      const jobs = req.body;
+      const result = await jobCollection.insertOne(jobs);
+      res.send(result);
+    });
 
-    })
-
-    app.post("/jobs",async(req,res)=>{
-        const jobs=req.body;
-        const result=await jobCollection.insertOne(jobs);
-        res.send(result);
-    })
-
-
-    app.get("/jobs/:id",async(req,res)=>{
-        const id=req.params.id;
-        const query={_id:new ObjectId(id)};
-        const result=await jobCollection.findOne(query);
-        res.send(result)
-    })
-
-
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobCollection.findOne(query);
+      res.send(result);
+    });
 
     // appliction apis
-    app.post("/applications",async(req,res)=>{
-        const appliction=req.body;
-        const result=await applicaitonsCollection.insertOne(appliction);
-        res.send(result);
-    })
+    app.post("/applications", async (req, res) => {
+      const appliction = req.body;
+      const result = await applicaitonsCollection.insertOne(appliction);
+      res.send(result);
+    });
 
-    app.get("/applications",async(req,res)=>{
-        const cursor=applicaitonsCollection.find();
-        const result=await cursor.toArray();
-        res.send(result);
-    })
-
+    app.get("/applications", async (req, res) => {
+      const cursor = applicaitonsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally { 
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("career code is running!");
+});
 
-app.get("/",(req,res)=>{
-    res.send("career code is running!")
-})
-
-app.listen(port,()=>{
-    console.log(`carrer code is runngin on port:${port}`);
-})
+app.listen(port, () => {
+  console.log(`carrer code is runngin on port:${port}`);
+});
